0005510f0f83c7a5401bc628fddb68e19bc9927e
sebas777 <sebastiancast06@hotmail.com>
2025-06-18 14:06:08 -0500

    [SC]feat:add projects

---
+++ Projects/springBootWeb/src/test/java/com/tutorial/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
