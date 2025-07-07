363d1cab5f70b470ede03882e26c6c45fe4d8466
sebas777 <sebastiancast06@hotmail.com>
2025-07-07 18:28:47 -0500

    [sc]componets angular

--- Angular/secondApp/src/app/app.component.ts
+++ Angular/secondApp/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [NuevoComponenteComponent]
 })
 export class AppComponent {
   title = 'Hola pendejos desde Angular!';
