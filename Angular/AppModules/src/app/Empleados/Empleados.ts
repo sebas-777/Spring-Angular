87d5a762876bb7f5ace0217226c3a842167cda95
sebas777 <sebastiancast06@hotmail.com>
2025-07-31 18:23:32 -0500

    [sc]feat:estudiando componentes

---
+++ Angular/AppModules/src/app/Empleados/Empleados.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-Empleados',
+    templateUrl: './Empleados.html',
+    standalone: false,
+    styleUrls: ['./Empleados.css']
+})
+
+export class Empleados{ 
+
+
+}
--- Angular/AppModules/src/app/app-module.ts
+++ Angular/AppModules/src/app/app-module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Empleados } from './Empleados/Empleados';
+import { Empleado } from './empleado/empleado';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Empleados,
+    Empleado
   ],
   imports: [
     BrowserModule,
